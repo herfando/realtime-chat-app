95358fc8076ea72233c1e6f50e90436e16323c92
Herfando <herfando_91@yahoo.com>
2025-09-08 16:08:11 +0700

    edit auth/page.tsx

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,3 +3,9 @@
 import { useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
+
+export default function AuthPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
